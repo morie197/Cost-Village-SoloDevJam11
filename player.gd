02a90f758e2d2eb153d9aa357912968e6ea75f5a
morie197 <snakeathon@gmail.com>
2026-05-10 04:43:41 -0400

    [Add] Camera and player controller
    Added a basic camera that can move and zoom in. Has a limited circular radius it can move within.

---
+++ player.gd
@@ -0,0 +1,61 @@
+extends Node2D
+
+@onready var camera = %Camera2D
+
+var speed: float = 500
+var zoom_min: float = 1
+var zoom_max: float = 3
+
+var zoom_in_amount: float = 0.8
+var zoom_out_amount: float = 1.4
+
+var scroll_amount: float = 1
+
+var max_distance_from_center: float = 500
+var center_position: Vector2
+
+func _unhandled_input(event):
+	if Input.is_action_pressed("zoom_in"):
+		scroll_amount = zoom_in_amount
+	elif Input.is_action_pressed("zoom_out"):
+		scroll_amount = zoom_out_amount
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	center_position = global_position
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	_zoom(delta)
+	
+func _physics_process(delta):
+	_move(delta)
+	
+func _move(_delta):
+	var move_input: Vector2 = Input.get_vector("left", "right", "up", "down")
+	if move_input == Vector2.ZERO:
+		return
+		
+	#print(center_position)
+		
+	var tween = create_tween()
+	var move_amount: Vector2 = move_input * (speed / camera.zoom.x) * _delta
+	var move_pos: Vector2 = global_position + move_amount
+	var offset_from_center: Vector2 = move_pos - center_position
+	
+#	print(offset_from_center)
+	
+	if offset_from_center.length() > max_distance_from_center:
+		offset_from_center = offset_from_center.limit_length(max_distance_from_center)
+	#global_position = conscious_pos + offset_from_conscious
+	#else:
+	tween.tween_property(self, "global_position", center_position + offset_from_center, _delta)
+		
+func _zoom(_delta):
+	if scroll_amount != 1.0:
+		var new_zoom = clampf(camera.zoom.x * scroll_amount, zoom_min, zoom_max)
+		
+		scroll_amount = 1.0
+		
+		var tween = create_tween()
+		tween.tween_property(camera, "zoom", Vector2(new_zoom, new_zoom), 0.1)
