0276936530facfe3f03f41fccf1a0879b614ea67
morie197 <snakeathon@gmail.com>
2026-05-09 23:56:37 -0400

    [Add] Basic pathfinding
    Added in basic pathfinding to a target position.

--- npc.gd
+++ npc.gd
@@ -8,6 +8,8 @@ class_name NPC
 
 @export var npc_visual: AtlasTexture
 
+@export var speed = 20
+
 var current_state: GameManager.npc_possible_states = GameManager.npc_possible_states.IDLE
 
 var state_time: float = 0
@@ -19,6 +21,8 @@ var update_rate_seconds: float = 0
 
 var target_coords: Vector2 = Vector2.ZERO
 var target_object: Node2D = null
+
+var target_achieve_distance: float = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -32,7 +36,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	_target_checks()
 
 func _physics_process(delta):
 	accumulator += delta
@@ -40,6 +44,17 @@ func _physics_process(delta):
 	if accumulator >= update_rate_seconds:
 		accumulator = 0
 		_update_npc()
+	_move()
+	
+func _target_checks() -> bool:
+	if target_coords != Vector2.ZERO:
+		if target_coords.distance_to(global_position) < target_achieve_distance:
+			target_coords = Vector2.ZERO
+			return true
+	else:
+		target_coords = GameManager.house_manager.houses.values()[1].global_position
+		agent.target_position = target_coords
+	return false
 	
 func _update_npc():
 	match current_state:
@@ -53,7 +68,10 @@ func _update_npc():
 			_fighting()
 			
 func _idle():
-	print("idle")
+	pass
+	#if _target_checks():
+		#state_time
+		
 	
 func _going_to():
 	print("going to")
@@ -64,3 +82,9 @@ func _inside():
 func _fighting():
 	print("fighting")
 	
+func _move():
+	var next_pos = agent.get_next_path_position()
+	var moveVector = (next_pos - global_position).normalized()
+	velocity = moveVector * speed
+	
+	move_and_slide()
